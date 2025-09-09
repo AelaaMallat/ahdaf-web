1ca1aac9f66ba76e755c9bb1403c5709e54b2c64
AlaaMallat <140802536+AlaaMallat@users.noreply.github.com>
2025-09-09 16:31:10 +0300

    update

--- pages/home/videos.tsx
+++ pages/home/videos.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const YouTubeVideoPage: React.FC = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="w-full max-w-4xl p-6">
+      <div className="w-full max-w-4xl p-6 bg-red-50">
         <h1 className="text-4xl font-bold text-center text-green-500 mb-8">
         Gallery
         </h1>
@@ -13,7 +13,29 @@ const YouTubeVideoPage: React.FC = () => {
           <iframe
             className="w-full h-full"
             src="https://www.youtube.com/embed/PTVJZIikQJg"
-            title="YouTube video player"
+            title="YouTube video 1"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+        
+        <h2 className="text-2xl font-bold text-center text-[#102F5C] mt-10">How Communities Perceive Charitable Endowment?</h2>
+        <div className="relative overflow-hidden rounded-lg aspect-video mt-6">
+          <iframe
+            className="w-full h-full"
+            src="https://www.youtube.com/embed/IA1MhHzGyVU"
+            title="YouTube video 2"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+
+        <h2 className="text-2xl font-bold text-center text-[#102F5C] mt-10">Annual Ramadan Gathering</h2>
+        <div className="relative overflow-hidden rounded-lg aspect-video mt-6">
+          <iframe
+            className="w-full h-full"
+            src="https://www.youtube.com/embed/WeiR-FYucC4"
+            title="YouTube video 3"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           ></iframe>
